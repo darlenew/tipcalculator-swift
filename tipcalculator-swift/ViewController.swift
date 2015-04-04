ccd63e3f98916c185d92a39c89d5e8c7e625add8
Darlene Wong <darlene.py@gmail.com>
2015-04-04 11:46:44 -0700

    Initial Commit

---
+++ tipcalculator-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalculator-swift
+//
+//  Created by Darlene Wong on 4/4/15.
+//  Copyright (c) 2015 Darlene Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
