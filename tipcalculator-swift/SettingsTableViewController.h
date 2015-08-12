270948a4559d8b4838eafa068c7e161b6487b544
Darlene Wong <darlene.py@gmail.com>
2015-08-11 22:03:45 -0700

    Modify storyboard, change Settings page from UIViewController to TableViewController.  Connect table cells to separate Tax and Tip UIViewControllers.

---
+++ tipcalculator-swift/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  tipcalculator-swift
+//
+//  Created by Darlene Wong on 8/11/15.
+//  Copyright (c) 2015 Darlene Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
